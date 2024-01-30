38ab2d8525244a5d8b146457ff242e6eb601f0f5
Magfur <33932322+mjafory@users.noreply.github.com>
2024-01-30 11:42:06 -0500

    Initial commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,20 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.Button
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        val button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            Log.v("Hello", "Button Clicked!")
+            Toast.makeText(this, "Hello World to you too", LENGTH_SHORT).show()
+        }
+    }
+}
